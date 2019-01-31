e3c8c0a5b6263b64477cc97d478abb0dd7df5ab1
Leonardo R. Martins <leonardo5621@gmail.com>
2019-01-31 21:04:24 -0200

    Added a Conversion method for mp3 files to wav

---
+++ src/Speaker_Verification/format_to_wav.py
@@ -0,0 +1,32 @@
+import glob
+import os
+from pydub import AudioSegment
+
+def ConvertWAV(audioPath,inpFormat,OneFile=True):
+    "If the objective is to convert several files at once, audioPath should be the path"
+    "to a directory, otherwise it should give the path directly to the file"
+    
+    currentDir=os.getcwd()
+    if OneFile:
+        audioDir,audioFile=os.path.split(audioPath)
+        os.chdir(audioDir)
+        baseName=os.path.splitext(os.path.basename(audioPath))[0]
+        newFileName=baseName+'.wav'
+        AudioSegment.from_file(audioFile).export(newFileName,format='wav')
+        os.chdir(currentDir)
+    else:
+        ##List of the mp3 files
+        audioList=glob.glob(audioPath+'*.'+inpFormat)
+        ##Changing to the directory containing the mp3 files
+        os.chdir(os.path.dirname(audioPath))
+        for audioFile in audioList:
+            ##Name of the Audio File
+            bs=os.path.basename(audioFile)
+            FileName=os.path.split(audioFile)[1]
+            baseFileName=os.path.splitext(FileName)[0]
+            ##Name of the New file
+            newFileName=baseFileName+'.wav'
+            AudioSegment.from_file(bs,format=inpFormat).export(newFileName,format='wav')
+        os.chdir(currentDir)
+
+            
