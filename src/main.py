cdd64c90b21a2238491dc40e314ffdf0499c978c
leonardo5621 <leonardo5621@gmail.com>
2019-02-24 18:58:10 -0300

    Added Microphone input option for verification

--- src/main.py
+++ src/main.py
@@ -1,18 +1,21 @@
 import argparse
+import sounddevice as sd
+from scipy.io import wavfile
 import Speaker_Verification.Training_Verification as GND
 import Speaker_Verification.FeatureExtraction as FtE
 import Speaker_Verification.format_to_wav as fileConvert
 def get_arguments():
 
     parser=argparse.ArgumentParser()
-    parser.add_argument('option',help='(train/verify) Choose between training a new model or performing the verification of an audio')
+    #parser.add_argument('option',help='(train/verify) Choose between training a new model or performing the verification of an audio')
     #parser.add_argument('-UBM','--UniversalModel',help='Choice of which Universal model to be used(Required for Verification)')
     parser.add_argument('Speaker',help='Speaker Id to verify or train a new model')
-    parser.add_argument('Audio',help='Audio File for Verification or Directory for Training')
+    parser.add_argument('-aud','--Audio',default='audioDef',help='Audio File for Verification or Directory for Training if file option was selected on -audm argument')
     parser.add_argument('-TT','--TrainedThresholds',help='Use the trained threshold for the speaker model instead of the standard adopted, which is zero'
                         ,action='store_true')
     parser.add_argument('-ff','--fileformat',default='wav',help='Format of the Audio File(Default=wav)')
     parser.add_argument('-lf','--listfiles',action='store_true',help='if there is a list of files being processed')
+    parser.add_argument('Audm',help='Method for delivering the audio to perform the verification(file/microphone)')
     return parser.parse_args()
 
 
@@ -26,13 +29,17 @@ def Main():
     islist=arguments.listfiles
     if formatAudio!='wav':
         fileConvert.ConvertWAV(AudioF,formatAudio,OneFile=islist)
-    if arguments.option=='train':
-        GND.TrainModel(AudioF,SpeakerId)
-    elif arguments.option=='verify':
+    if arguments.Audm=='file':
         GND.Verification(SpeakerId,AudioF,TrainedThresh=TT)
+    elif arguments.Audm=='microphone':
+        recording=sd.rec(int(44100*5),samplerate=44100,channels=2)
+        print('RECORDING')
+        sd.wait()
+        print('Recording Finished')
+        wavfile.write('Verify.wav',44100,recording)
+        GND.Verification(SpeakerId,'Verify.wav',TrainedThresh=TT)
     else:
-        print('Invalid Option')
-
+        print('Invalid Method')
 
 if __name__=='__main__':
     
