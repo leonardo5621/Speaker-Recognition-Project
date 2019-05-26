5468e70eb3d03f53bc9cfa322a5ebeae6350f82c
leonardo5621 <leonardo5621@gmail.com>
2019-05-26 10:30:39 -0300

    Signal Created For Cleaning Up Files

--- WebApp/Voice_App/create_gmm/signals.py
+++ WebApp/Voice_App/create_gmm/signals.py
@@ -1,5 +1,6 @@
+import os
 from .Training_Verification import Train_Model
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import AcousticModel
@@ -14,3 +15,15 @@ def create_model(sender , instance, created, **kwargs):
         print('Creating Acoustic Model')
         Train_Model(Train_Dir, Model_name, s_rate, AudioFormat=Audio_format)
 
+@receiver(pre_delete, sender=AcousticModel)
+def delete_file(sender , instance, **kwargs):
+    if instance.audio_data:
+        if os.path.isfile(instance.audio_data.path):
+            dir_name = os.path.dirname(instance.audio_data.path)
+            os.remove(instance.audio_data.path)
+
+            model_path = '{0}{1}.model'.format(dir_name, instance.user)
+            if os.path.isfile(model_path):
+                os.remove(model_path)
+            
+        
