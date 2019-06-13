33e08a098d4e40902bb6ccc049e5076e05819ab3
leonardo5621 <leonardo5621@gmail.com>
2019-06-12 22:08:21 -0300

    Password Recovery feature added

--- Voice_App/Voice_App/settings.py
+++ Voice_App/Voice_App/settings.py
@@ -128,8 +128,15 @@ LOGIN_REDIRECT_URL = 'home'
 
 #LOGOUT_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'voiceapp.recover@gmail.com' 
+EMAIL_HOST_PASSWORD = 'myappdeploy123'
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Voice_App/Voice_App/urls.py
+++ Voice_App/Voice_App/urls.py
@@ -28,9 +28,18 @@ urlpatterns = [
 #    path('', TemplateView.as_view(template_name='accounts/home_template.html'), name='home'),
     path('about', TemplateView.as_view(template_name='accounts/About.html'), name='about'),
     path('gmmodels/', include('create_gmm.urls')),
-    path('', include('content.urls'))
-    
+    path('', include('content.urls')),
+    path('password-reset/',
+    auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password-reset/done/',
+    auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+    auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
+    
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
